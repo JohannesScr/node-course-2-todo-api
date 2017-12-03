ed8f47aa14e84c83eb763426926a9ae1c1ec80fe
Johannes Scribante <johannesscr@Johanness-MacBook.local>
2017-12-03 12:17:55 +0200

    feat - added the POST /todo route.

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const Express = require('express');
+const body_parser = require('body-parser');
+
+let server_settings_service = require('./server.settings');
+let {mongoose} = require('./db/mongoose');
+let {Todo} = require('./model/todo');
+let {User} = require('./model/user');
+
+const app = Express();
+
+const PORT = 3000;
+
+app.use(body_parser.json());
+app.use(server_settings_service.log_url);
+app.use(server_settings_service.add_result_object);
+
+// #### TODO
+app.post('/todo', (req, res) => {
+
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save()
+            .then((doc) => {
+                req.result.data = doc;
+                res.send(req.result);
+            })
+            .catch((err) => {
+                console.log('Unable to save todo: ', err);
+                req.result.http_code = 400;
+                req.result.message = 'Bad Request: ' + err.message;
+                req.result.errors.push(err.message);
+
+                res.status(req.result.http_code).send(req.result);
+            });
+});
+
+// #### USER
+
+
+app.listen(PORT, () => {
+    console.log(`Express App successful listening on port: ${PORT}`);
+});
---
+++ server/server.settings.js
@@ -0,0 +1,17 @@
+// ADD ALL SERVER SETTINGS HERE
+
+exports.log_url = (req, res, next) => {
+    console.log(req.url);
+    next();
+};
+
+exports.add_result_object = (req, res, next) => {
+    req.result = {
+        http_code: 200,
+        error_code: '',
+        message: '',
+        data: {},
+        errors: []
+    };
+    next();
+};
